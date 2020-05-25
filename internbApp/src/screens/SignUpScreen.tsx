4998467bd9fd0f1268edb87feb1bd1c8f30351a1
JomaSnow <jm.duda@uol.com.br>
2020-05-25 19:28:26 -0300

    Adiciona suporte para enviar dados para api

--- internbApp/src/screens/SignUpScreen.tsx
+++ internbApp/src/screens/SignUpScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SignUpBackground as Background} from '../components/LoginBackground';
 import Title, {TitleHeader} from '../components/AppTitle';
 import {Column, Row} from '../components/Layout';
@@ -6,9 +6,27 @@ import PageTitle from '../components/PageTitle';
 import Container from '../components/Container';
 import FormInput from '../components/FormInput';
 import FormButton from '../components/FormButton';
-import Logo from '../components/Logo';
 
 const SignUpScreen = () => {
+  const [nome, setNome] = useState<string>('');
+  const [cpf, setCpf] = useState<string>('');
+  const [telefone, setTelefone] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [senha, setSenha] = useState<string>('');
+  const [senhaConfirma, setSenhaConfirma] = useState<string>('');
+
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setLoading(true);
+    console.log(
+      `Cadastrando Nome: ${nome}, CPF: ${cpf}, Telefone: ${telefone}, E-mail: ${email}, Senha: ${senha}, Confirma Senha: ${senhaConfirma}`,
+    );
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  };
+
   return (
     <>
       <Background>
@@ -26,13 +44,53 @@ const SignUpScreen = () => {
           <Column width="90%">
             <Container width="100%" height="70%">
               <Column height="100%" justifyContent="space-evenly">
-                <FormInput inlineLabel={true} label="Nome" />
-                <FormInput inlineLabel={true} label="CPF" />
-                <FormInput inlineLabel={true} label="Telefone" />
-                <FormInput inlineLabel={true} label="E-mail" />
-                <FormInput inlineLabel={true} label="Senha" />
-                <FormInput inlineLabel={true} label="Confirmar Senha" />
-                <FormButton buttonText="Cadastrar" onPress={() => {}} />
+                <FormInput
+                  inlineLabel={true}
+                  label="Nome"
+                  field={nome}
+                  setField={setNome}
+                  isProtected={false}
+                />
+                <FormInput
+                  inlineLabel={true}
+                  label="CPF"
+                  field={cpf}
+                  setField={setCpf}
+                  isProtected={false}
+                />
+                <FormInput
+                  inlineLabel={true}
+                  label="Telefone"
+                  field={telefone}
+                  setField={setTelefone}
+                  isProtected={false}
+                />
+                <FormInput
+                  inlineLabel={true}
+                  label="E-mail"
+                  field={email}
+                  setField={setEmail}
+                  isProtected={false}
+                />
+                <FormInput
+                  inlineLabel={true}
+                  label="Senha"
+                  field={senha}
+                  setField={setSenha}
+                  isProtected={true}
+                />
+                <FormInput
+                  inlineLabel={true}
+                  label="Confirmar Senha"
+                  field={senhaConfirma}
+                  setField={setSenhaConfirma}
+                  isProtected={true}
+                />
+                <FormButton
+                  buttonText="Cadastrar"
+                  onPress={handleClick}
+                  isLoading={loading}
+                />
               </Column>
             </Container>
           </Column>
