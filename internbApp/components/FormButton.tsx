c1ce9fa91fa0636c17b620edb2420b3805535a62
JomaSnow <jm.duda@uol.com.br>
2020-05-25 03:38:55 -0300

    Adiciona logica de loading para o botao

--- internbApp/components/FormButton.tsx
+++ internbApp/components/FormButton.tsx
@@ -1,17 +1,40 @@
 import React from 'react';
-import {StyleSheet, TouchableOpacity, Text} from 'react-native';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  ActivityIndicator,
+} from 'react-native';
+import {Row} from './Layout';
 
 interface Props {
   buttonText: string;
   onPress: any;
+  isLoading?: boolean;
 }
 
-const FormButton: React.FC<Props> = ({buttonText, onPress}) => {
+const FormButton: React.FC<Props> = ({
+  buttonText,
+  onPress,
+  isLoading = false,
+}) => {
+  let buttonStyle = styles;
+
+  if (isLoading === true) {
+    buttonStyle = disabledButton;
+  }
+
   return (
     <>
-      <TouchableOpacity style={styles.button} onPress={onPress}>
-        <Text style={styles.text}>{buttonText}</Text>
-      </TouchableOpacity>
+      <Row>
+        <TouchableOpacity
+          style={buttonStyle.button}
+          onPress={onPress}
+          disabled={isLoading}>
+          <Text style={buttonStyle.text}>{buttonText}</Text>
+        </TouchableOpacity>
+        <ActivityIndicator animating={isLoading} color="#216621" />
+      </Row>
     </>
   );
 };
@@ -26,13 +49,38 @@ const styles = StyleSheet.create({
     borderColor: '#1c1e83',
     borderWidth: 3,
     borderRadius: 10,
-    width: '25%',
+    width: 90,
     height: 25,
+    marginRight: 10,
+    marginLeft: 10,
   },
   text: {
     fontWeight: 'bold',
     color: '#000000',
     letterSpacing: 1,
+  },
+});
+
+const disabledButton = StyleSheet.create({
+  button: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+    borderColor: '#1c1e8366',
+    borderWidth: 3,
+    borderRadius: 10,
+    width: 90,
+    height: 25,
+    marginRight: 10,
+    marginLeft: 10,
+  },
+  text: {
+    fontWeight: 'bold',
+    color: '#000000',
+    letterSpacing: 1,
+    opacity: 0.4,
   },
 });
 
