2c792771c290e9a8a104d8d1c2ca57003607bc20
JomaSnow <jm.duda@uol.com.br>
2020-07-14 04:22:06 -0300

    Creates Navbar Component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,32 +1,17 @@
 import React from 'react';
 
-import { Text, TouchableOpacity } from 'react-native';
+import { Container, Body } from './styles';
 
-import { Container } from './styles';
-
-import { useAuth } from '../../hooks/auth';
+import Navbar from '../../components/Navbar';
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
-
   return (
-    <Container>
-      <Text>Hello, Dashboard</Text>
-      <TouchableOpacity
-        onPress={async () => {
-          await signOut();
-        }}
-        style={{
-          marginTop: 10,
-          borderColor: '#333',
-          borderWidth: 1,
-          borderRadius: 10,
-          padding: 10,
-        }}
-      >
-        <Text>Logout</Text>
-      </TouchableOpacity>
-    </Container>
+    <>
+      <Container>
+        <Body />
+        <Navbar currentTab={2} />
+      </Container>
+    </>
   );
 };
 
