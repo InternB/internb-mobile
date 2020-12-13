3b7f75041f5adba6dde692d795de1d3a4b04dab9
Bernardo Costa Nascimento <bernardoc1104@gmail.com>
2020-12-13 11:19:15 -0300

    hook de upload completo

--- src/hooks/upload.tsx
+++ src/hooks/upload.tsx
@@ -1,10 +1,30 @@
+/* eslint-disable camelcase */
+
+import 'react-native-get-random-values';
 import React, { createContext, useCallback, useContext } from 'react';
 import storage from '@react-native-firebase/storage';
-import { launchImageLibrary } from 'react-native-image-picker';
-import fs from 'react-native-fs';
+import { v4 } from 'uuid';
+
+import api from '../services/api';
+
+interface UploadFileOptions {
+  internship_id: string;
+  file: string;
+  storageFolder: 'compromises' | 'work-plans' | 'avatars';
+}
+
+interface UploadContractsOptions {
+  internship_id: string;
+  data: {
+    first_copy: string;
+    second_copy: string;
+    third_copy: string;
+  };
+}
 
 interface UploadContextState {
-  upload(dir: string, filename: string): Promise<void>;
+  uploadFile(uploadOptions: UploadFileOptions): Promise<void>;
+  uploadContracts(uploadOptions: UploadContractsOptions): Promise<void>;
 }
 
 const UploadContext = createContext<UploadContextState>(
@@ -12,28 +32,66 @@ const UploadContext = createContext<UploadContextState>(
 );
 
 export const UploadProvider: React.FC = ({ children }) => {
-  const upload = useCallback(
-    async (dir: string, filename: string): Promise<void> => {
-      // TODO
-      const reference = storage().ref('black-t-shirt-sm.png');
-      launchImageLibrary({ mediaType: 'photo' }, async (response) => {
-        if (response.uri) {
-          try {
-            const data = await fs.readFile(response.uri!, 'base64');
-            await reference.putString(data, 'base64');
-          } catch (e) {
-            console.log(e);
-          }
-        }
-      });
+  const uploadFile = useCallback(
+    async ({
+      internship_id,
+      file,
+      storageFolder,
+    }: UploadFileOptions): Promise<void> => {
+      const filename = v4();
 
-      console.log(dir, filename);
+      const ref = storage().ref(`${storageFolder}/${filename}`);
+
+      try {
+        await ref.putString(file, 'base64');
+
+        if (storageFolder !== 'avatars') {
+          await api.patch(`internships/upload/${storageFolder}`, {
+            internship_id,
+            [storageFolder]: filename,
+          });
+        } else {
+          await api.patch(`profiles/`, {
+            avatar: filename,
+          });
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    [],
+  );
+
+  const uploadContracts = useCallback(
+    async ({ internship_id, data }: UploadContractsOptions) => {
+      const first_filename = v4();
+      const second_filename = v4();
+      const third_filename = v4();
+
+      const first_ref = storage().ref(`contracts/${first_filename}`);
+      const second_ref = storage().ref(`contracts/${second_filename}`);
+      const third_ref = storage().ref(`contracts/${third_filename}`);
+
+      try {
+        await first_ref.putString(data.first_copy, 'base64');
+        await second_ref.putString(data.second_copy, 'base64');
+        await third_ref.putString(data.third_copy, 'base64');
+
+        await api.patch('internships/upload/contracts', {
+          internship_id,
+          first_copy: first_filename,
+          second_copy: second_filename,
+          third_copy: third_filename,
+        });
+      } catch (err) {
+        console.log(err);
+      }
     },
     [],
   );
 
   return (
-    <UploadContext.Provider value={{ upload }}>
+    <UploadContext.Provider value={{ uploadFile, uploadContracts }}>
       {children}
     </UploadContext.Provider>
   );
